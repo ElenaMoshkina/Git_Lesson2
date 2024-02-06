859662ceeb9b954ec1f9d4e0f3b25afd93d188b1
Elena Moshkina <8952559elena@gmail.com>
2024-02-06 22:03:21 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+фамилию можешь на английском добавить если нужно
+
+git config --local user.email "8952559elena@gmail.com"
+есть этот проект на гитхабе?
+неа
+ок
